6991de53b5ee14cebca9887e33ac9ccde8a286a6
Dhilipkumar-max <dilippkumarmax@gmail.com>
2026-07-16 17:24:02 +0530

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Memory_Card/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
